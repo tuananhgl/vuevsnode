2938253a92cf7c31bef8d96fbe23a3bd6499e948
tuananhsmart17 <tuananh.smart17@gmail.com>
2018-04-11 18:42:32 +0700

    first commit

--- vuetest/src/components/Login.vue
+++ vuetest/src/components/Login.vue
@@ -1,6 +1,25 @@
 <template>
-  <div id="app">
+  <div id="loginpage">
       <h1 class="title">{{ex}}</h1>
+      <br>
+      <b-container class="bv-example-row">
+        <b-col cols="5" align-h="center" style="display:block;margin: 0 auto;">
+          <b-col>
+              <b-form @submit.prevent="submitForm(form)">
+                <b-form-group id="exampleInputGroup1" label="Email address:" label-for="exampleInput1" description="We'll never share your email with anyone else.">
+                  <b-form-input id="exampleInput1" type="email" v-model="form.email" required placeholder="Enter email">
+                  </b-form-input>
+                </b-form-group>
+                <b-form-group id="exampleInputGroup1" label="Password:" label-for="exampleInput1" >
+                  <b-form-input id="exampleInput1" type="password" v-model="form.email" required placeholder="Enter password">
+                  </b-form-input>
+                </b-form-group>
+                <b-button type="submit" variant="primary">Submit</b-button>
+                <b-button type="reset" variant="danger">Reset</b-button>
+              </b-form>
+          </b-col>
+        </b-col>
+      </b-container>
   </div>
 </template>
 
@@ -9,14 +28,23 @@ export default {
   name: 'LoginVue',
   data () {
     return {
-      ex: 'Canceled'
+      ex: 'Login To System',
+      form: {}
+    }
+  },
+  methods: {
+    submitForm: async function (val) {
     }
   }
 }
 </script>
 
 <style scoped>
+  #loginpage {
+    font-family: monospace !important;
+  }
   .title {
       display: block;
+      font-family: monospace;
   }
 </style>
--- vuetest/src/main.js
+++ vuetest/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import './assets/fonts/Fira.eot'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
